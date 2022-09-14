97cf2bad958be8007aeb595e996f647df1cce302
Anri Sakakibara <anrisakaki@gmail.com>
2022-09-14 09:53:06 +0100

    Removing unnecessary weights objects

--- Weights data.R
+++ Weights data.R
@@ -11,19 +11,11 @@ weights_02 <- inc_02 %>%
   distinct() %>% 
   mutate(xa02 = tinh02 * 10^4 + huyen02 * 10^2 + xa02)
 
-weights_exp_02 <- exp_02 %>% 
-  select(tinh02, xa02, hhid, wt30) %>% 
-  distinct() %>% 
-  rename(hhwt = wt30)
-
 # 2004 
 weights_04 <- inc_04 %>%
   select(tinh, huyen, xa, wt45, urban) %>% 
   rename(hhwt = wt45) %>% 
   distinct() 
-
-weights_exp_04 <- exp_04 %>% 
-  select(hhid, hhwt)
 
 # 2006 
 weights_06 <- exp_06 %>%
@@ -31,7 +23,3 @@ weights_06 <- exp_06 %>%
   rename("urban" = urban06,
          "hhwt" = wt45) %>% 
   distinct()  
-
-weights_exp_06 <- exp_06 %>% 
-  select(hhid, wt9) %>% 
-  rename(hhwt = wt9)
