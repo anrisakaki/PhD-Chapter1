9a5b8905a6b440e60855a04f01723d07288c6f1f
Anri Sakakibara <anrisakaki@gmail.com>
2022-09-14 09:52:20 +0100

    Weights data

--- Weights data.R
+++ Weights data.R
@@ -2,4 +2,36 @@
 # SETTING UP DATA FOR WEIGHTS #
 ###############################
 
+# These are weights for cross-sectional observations 
+
 # 2002 
+weights_02 <- inc_02 %>%
+  select(tinh02, xa02, diaban02, huyen02, wt75, urban) %>% #Weights are determined by tinh, xa, diaban, huyen
+  rename(hhwt = wt75) %>% 
+  distinct() %>% 
+  mutate(xa02 = tinh02 * 10^4 + huyen02 * 10^2 + xa02)
+
+weights_exp_02 <- exp_02 %>% 
+  select(tinh02, xa02, hhid, wt30) %>% 
+  distinct() %>% 
+  rename(hhwt = wt30)
+
+# 2004 
+weights_04 <- inc_04 %>%
+  select(tinh, huyen, xa, wt45, urban) %>% 
+  rename(hhwt = wt45) %>% 
+  distinct() 
+
+weights_exp_04 <- exp_04 %>% 
+  select(hhid, hhwt)
+
+# 2006 
+weights_06 <- exp_06 %>%
+  select(tinh, huyen, xa, wt45, urban06) %>% 
+  rename("urban" = urban06,
+         "hhwt" = wt45) %>% 
+  distinct()  
+
+weights_exp_06 <- exp_06 %>% 
+  select(hhid, wt9) %>% 
+  rename(hhwt = wt9)
