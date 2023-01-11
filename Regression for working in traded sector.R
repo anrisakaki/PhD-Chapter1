874cd245c8091ff8f7a5d99eeb9e3136d99cb427
Anri Sakakibara <anrisakaki@gmail.com>
2023-01-11 16:25:22 +0000

    Regression for working in traded non-manufacturing sector

--- Regression for working in traded sector.R
+++ Regression for working in traded sector.R
@@ -1,29 +1,29 @@
-############################################################
-# REGRESSION FOR WORKING IN TRADED SECTOR USING PANEL DATA #
-############################################################
+##########################################################################
+# REGRESSION FOR WORKING IN TRADED MANUFACTURING SECTOR USING PANEL DATA #
+##########################################################################
 
-employment0204_exc_agri_p <- employment0204_p %>% 
-  filter(!(agri_work == 1 & year == 2004))
+employment0204_p <- employment0204_p %>% 
+  mutate(traded_manu = as.numeric(traded == 1 & manu == 1))
 
-employment0206_exc_agri_p <- employment0206_p %>% 
-  filter(!(agri_work == 1 & year == 2006))
+employment0206_p <- employment0206_p %>% 
+  mutate(traded_manu = as.numeric(traded == 1 & manu == 1))
 
 etable(list(
   feols(
-    traded ~ factor(sex)*provtariff | year + ivid,
-    employment0204_exc_agri_p,
+    traded_manu ~ factor(sex)/provtariff | year + ivid,
+    employment0204_p,
     vcov = ~tinh,
     weights = ~hhwt),
-  feols(traded ~ factor(sex)*provtariff_k | year + ivid,
-        employment0204_exc_agri_p,
+  feols(traded_manu ~ factor(sex)/provtariff_k | year + ivid,
+        employment0204_p,
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(traded ~ factor(sex)*provtariff | year + ivid02,
-        employment0206_exc_agri_p,
+  feols(traded_manu ~ factor(sex)/provtariff | year + ivid02,
+        employment0206_p,
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(traded ~ factor(sex)*provtariff_k | year + ivid02,
-        employment0206_exc_agri_p,
+  feols(traded_manu ~ factor(sex)/provtariff_k | year + ivid02,
+        employment0206_p,
         vcov = ~tinh,
         weights = ~hhwt)
   ),
