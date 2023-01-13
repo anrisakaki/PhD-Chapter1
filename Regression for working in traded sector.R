e853fef8531beefec1d2a5ed9ae851c782003b6a
Anri Sakakibara <anrisakaki@gmail.com>
2023-01-13 16:10:24 +0000

    Regression for working in traded sector

--- Regression for working in traded sector.R
+++ Regression for working in traded sector.R
@@ -32,3 +32,28 @@ etable(list(
 ######################################################################
 # REGRESSION FOR WORKING IN TRADED SECTOR USING CROSS-SECTIONAL DATA #
 ######################################################################
+
+employment0204 <- employment0204 %>% 
+  mutate(traded_manu = as.numeric(traded == 1 & manu == 1))
+
+employment0206 <- employment0206 %>% 
+  mutate(traded_manu = as.numeric(traded == 1 & manu == 1))
+
+etable(list(
+  feols(traded_manu ~ factor(sex)/provtariff + age + age^2  + educ + factor(urban) | year + tinh,
+        data = employment0204,
+        vcov = ~tinh,
+        weights = ~ hhwt),
+  feols(traded_manu ~ factor(sex)/provtariff_k + age + age^2  + educ + factor(urban) | year + tinh,
+        data = employment0204,
+        vcov = ~tinh,
+        weights = ~ hhwt),
+  feols(traded_manu ~ factor(sex)/provtariff + age + age^2  + educ + factor(urban) | year + tinh,
+        data = employment0206,
+        vcov = ~tinh,
+        weights = ~ hhwt),  
+  feols(traded_manu ~ factor(sex)/provtariff_k + age + age^2  + educ + factor(urban) | year + tinh,
+        data = employment0206,
+        vcov = ~tinh,
+        weights = ~ hhwt)  
+))
