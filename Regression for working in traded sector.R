d3787d930beacbecfe963acefe184cc5dfb84e8b
Anri Sakakibara <anrisakaki@gmail.com>
2023-01-31 15:59:49 +0000

    Regression for working in traded sector

--- Regression for working in traded sector.R
+++ Regression for working in traded sector.R
@@ -8,26 +8,72 @@ employment0204_p <- employment0204_p %>%
 employment0206_p <- employment0206_p %>% 
   mutate(traded_manu = as.numeric(traded == 1 & manu == 1))
 
+# Switching from agriculture pre-BTA to traded manufacturing post-BTA 
+
 etable(list(
   feols(
-    traded_manu ~ i(sex, provtariff) | year + ivid,
-    employment0204_p,
+    traded_manu ~ as.factor(Female)/provtariff | year + ivid,
+    subset(employment0204_p, agri_work == 1 & year == 2002 | year == 2004),
     vcov = ~tinh,
     weights = ~hhwt),
-  feols(traded_manu ~ i(sex, provtariff_k) | year + ivid,
-        employment0204_p,
+  feols(traded_manu ~ as.factor(Female)/provtariff_k | year + ivid,
+        subset(employment0204_p, agri_work == 1 & year == 2002 | year == 2004),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(traded_manu ~ i(sex, provtariff) | year + ivid02,
-        employment0206_p,
+  feols(traded_manu ~ as.factor(Female)/provtariff | year + ivid02,
+        subset(employment0206_p, agri_work == 1 & year == 2002 | year == 2006),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(traded_manu ~ i(sex, provtariff_k) | year + ivid02,
-        employment0206_p,
+  feols(traded_manu ~ as.factor(Female)/provtariff_k | year + ivid02,
+        subset(employment0206_p, agri_work == 1 & year == 2002 | year == 2006),
         vcov = ~tinh,
         weights = ~hhwt)
-  ),
-  tex = TRUE)
+),
+tex = TRUE)
+
+# Rural 
+etable(list(
+  feols(
+    traded_manu ~ as.factor(Female)/provtariff | year + ivid,
+    subset(employment0204_p, urban == 2 & work == 1),
+    vcov = ~tinh,
+    weights = ~hhwt),
+  feols(traded_manu ~ as.factor(Female)/provtariff_k | year + ivid,
+        subset(employment0204_p, urban == 2 & work == 1),
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(traded_manu ~ as.factor(Female)/provtariff | year + ivid02,
+        subset(employment0206_p, urban == 2 & work == 1),
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(traded_manu ~ as.factor(Female)/provtariff_k | year + ivid02,
+        subset(employment0206_p, urban == 2 & work == 1),
+        vcov = ~tinh,
+        weights = ~hhwt)
+),
+tex = TRUE)
+
+# Education 
+etable(list(
+  feols(
+    traded_manu ~ as.factor(Female)/provtariff | year + ivid,
+    subset(employment0204_p, educ < 10 & year == 2002 | year == 2004),
+    vcov = ~tinh,
+    weights = ~hhwt),
+  feols(traded_manu ~ as.factor(Female)/provtariff_k | year + ivid,
+        subset(employment0204_p, educ < 10 & year == 2002 | year == 2004),
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(traded_manu ~ as.factor(Female)/provtariff | year + ivid02,
+        subset(employment0206_p, educ < 10 & year == 2002 | year == 2006),
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(traded_manu ~ as.factor(Female)/provtariff_k | year + ivid02,
+        subset(employment0206_p, educ < 10 & year == 2002 | year == 2006),
+        vcov = ~tinh,
+        weights = ~hhwt)
+),
+tex = TRUE)
 
 ######################################################################
 # REGRESSION FOR WORKING IN TRADED SECTOR USING CROSS-SECTIONAL DATA #
