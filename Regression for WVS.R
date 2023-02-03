f62348daf09502302e0b5a378e03a69e46eb2014
Anri Sakakibara <anrisakaki@gmail.com>
2023-02-03 14:38:32 +0000

    Regression for WVS

--- Regression for WVS.R
+++ Regression for WVS.R
@@ -18,8 +18,68 @@ WVS_01 <- WVS_01 %>%
   rename(job_scarce = V78,
          working_mother = V115,
          housewife = V116,
-         both_contribute = V117)
+         both_contribute = V117,
+         provtariff = preprov_tariff,
+         provtariff_k = preprov_tariff_k, 
+         sex = V223,
+         age = V225, 
+         educ = v226) %>% 
+  mutate(female = as.numeric(sex > 1),
+         year = 2001) %>% 
+  select(tinh, provtariff, provtariff_k, female, age, educ, housewife)
 
 WVS_05 <- WVS_05 %>% 
   rename(job_scarce = V44,
-         housewife = V60)
+         housewife = V60,
+         provtariff = postprov_tariff,
+         provtariff_k = postprov_tariff_k,
+         sex = V235,
+         educ = V238,
+         age = V237) %>% 
+  mutate(female = as.numeric(sex > 1),
+         year = 2005) %>% 
+  select(tinh, provtariff, provtariff_k, female, age, educ, housewife)  
+
+WVS_0105 <- bind_rows(WVS_01, WVS_05) %>% 
+  mutate(housewife = as.numeric(housewife < 3))
+
+#####################
+# REGRESSION OF WVS #
+#####################
+
+etable(list(
+  feols(housewife ~ provtariff + as.factor(female) + age + as.factor(educ) | year + tinh,
+        WVS_0105, 
+        vcov = ~tinh),
+  feols(housewife ~ provtariff_k + as.factor(female) + age + as.factor(educ) | year + tinh,
+        WVS_0105, 
+        vcov = ~tinh)
+), tex = TRUE)
+
+
+etable(list(
+  feols(housewife ~ provtariff + age + as.factor(educ) | year + tinh,
+        subset(WVS_0105, female == 1), 
+        vcov = ~tinh),
+  feols(housewife ~ provtariff_k + age + as.factor(educ) | year + tinh,
+        subset(WVS_0105, female == 1), 
+        vcov = ~tinh),
+), tex = TRUE)
+
+etable(list(
+  feols(housewife ~ provtariff + as.factor(female) + age + as.factor(educ) | year + tinh,
+        subset(WVS_0105, educ <= 5), 
+        vcov = ~tinh),
+  feols(housewife ~ provtariff_k + as.factor(female) + age + as.factor(educ) | year + tinh,
+        subset(WVS_0105, educ <= 5), 
+        vcov = ~tinh)  
+  ), tex = TRUE)
+
+etable(list(
+  feols(housewife ~ provtariff + as.factor(female) + age + as.factor(educ) | year + tinh,
+        subset(WVS_0105, educ > 5), 
+        vcov = ~tinh),
+  feols(housewife ~ provtariff_k + as.factor(female) + age + as.factor(educ) | year + tinh,
+        subset(WVS_0105, educ > 5), 
+        vcov = ~tinh)  
+), tex = TRUE)
