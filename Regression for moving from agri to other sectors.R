6fba04861165bf9b73fa25ac5df20ee204e89e53
Anri Sakakibara <anrisakaki@gmail.com>
2023-02-07 12:42:02 +0000

    Regression for moving from the agricultural into other sectors

---
+++ Regression for moving from agri to other sectors.R
@@ -0,0 +1,44 @@
+###################################################################################
+# REGRESSION ON STRUCTURAL TRANSFORMATION FROM THE AGRICULTURE INTO OTHER SECTORS #
+###################################################################################
+
+# Manufacturing 
+etable(list(
+  feols(manu ~ as.factor(Female)/provtariff | year + ivid,
+        subset(employment0204_p, agri_work == 1 & year == 2002 | year == 2004),
+        vcov = ~tinh,
+        weights = ~hhwt),  
+  feols(manu ~ as.factor(Female)/provtariff_k | year + ivid,
+        subset(employment0204_p, agri_work == 1 & year == 2002 | year == 2004),
+        vcov = ~tinh,
+        weights = ~hhwt),   
+  feols(manu ~ as.factor(Female)/provtariff | year + ivid02,
+        subset(employment0206_p, agri_work == 1 & year == 2002 | year == 2006),
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(manu ~ as.factor(Female)/provtariff_k | year + ivid02,
+        subset(employment0206_p, agri_work == 1 & year == 2002 | year == 2006),
+        vcov = ~tinh,
+        weights = ~hhwt)), tex = TRUE
+)
+
+# Wearing apparel and leather 
+etable(list(
+  feols(tal ~ as.factor(Female)/provtariff | year + ivid,
+        subset(employment0204_p, agri_work == 1 & year == 2002 | year == 2004),
+        vcov = ~tinh,
+        weights = ~hhwt),  
+  feols(tal ~ as.factor(Female)/provtariff_k | year + ivid,
+        subset(employment0204_p, agri_work == 1 & year == 2002 | year == 2004),
+        vcov = ~tinh,
+        weights = ~hhwt),   
+  feols(tal ~ as.factor(Female)/provtariff | year + ivid02,
+        subset(employment0206_p, agri_work == 1 & year == 2002 | year == 2006),
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(tal ~ as.factor(Female)/provtariff_k | year + ivid02,
+        subset(employment0206_p, agri_work == 1 & year == 2002 | year == 2006),
+        vcov = ~tinh,
+        weights = ~hhwt), tex = TRUE)
+)
+
