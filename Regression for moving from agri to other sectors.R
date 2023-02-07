3aaf5324b902766d68356b5a1c98297b8b95de27
Anri Sakakibara <anrisakaki@gmail.com>
2023-02-07 14:15:18 +0000

    Regression from moving from the agricultural to other sectors

--- Regression for moving from agri to other sectors.R
+++ Regression for moving from agri to other sectors.R
@@ -22,6 +22,27 @@ etable(list(
         weights = ~hhwt)), tex = TRUE
 )
 
+# Traded-manufacturing 
+etable(list(
+  feols(traded_manu ~ as.factor(Female)/provtariff | year + ivid,
+        subset(employment0204_p, agri_work == 1 & year == 2002 | year == 2004),
+        vcov = ~tinh,
+        weights = ~hhwt),  
+  feols(traded_manu ~ as.factor(Female)/provtariff_k | year + ivid,
+        subset(employment0204_p, agri_work == 1 & year == 2002 | year == 2004),
+        vcov = ~tinh,
+        weights = ~hhwt),   
+  feols(traded_manu ~ as.factor(Female)/provtariff | year + ivid02,
+        subset(employment0206_p, agri_work == 1 & year == 2002 | year == 2006),
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(traded_manu ~ as.factor(Female)/provtariff_k | year + ivid02,
+        subset(employment0206_p, agri_work == 1 & year == 2002 | year == 2006),
+        vcov = ~tinh,
+        weights = ~hhwt)), tex = TRUE
+)
+
+
 # Wearing apparel and leather 
 etable(list(
   feols(tal ~ as.factor(Female)/provtariff | year + ivid,
@@ -39,6 +60,5 @@ etable(list(
   feols(tal ~ as.factor(Female)/provtariff_k | year + ivid02,
         subset(employment0206_p, agri_work == 1 & year == 2002 | year == 2006),
         vcov = ~tinh,
-        weights = ~hhwt), tex = TRUE)
-)
+        weights = ~hhwt)), tex = TRUE)
 
