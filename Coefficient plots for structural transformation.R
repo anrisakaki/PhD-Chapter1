c5cbe32e2bd39446b484bcb772170d7648d2e594
Anri Sakakibara <anrisakaki@gmail.com>
2023-02-23 13:40:53 +0000

    Coefficient plots for structural transformation

--- Coefficient plots for structural transformation.R
+++ Coefficient plots for structural transformation.R
@@ -1,3 +1,4 @@
+# All observations 
 iplot(list(
   feols(tal ~ i(Female, provtariff) | year + ivid,
         employment0204_p,
@@ -8,16 +9,23 @@ iplot(list(
         vcov = ~tinh,
         weights = ~hhwt)))
 
+# Age 
 iplot(list(
-  feols(tal ~ i(Female, provtariff) | year + ivid,
+  feols(tal ~ i(as.factor(Female), provtariff) | year + ivid,
         subset(employment0204_p, age > 17 & age < 31 & year == 2002 | year == 2004),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(tal ~ i(Female, provtariff) | year + ivid02,
-        subset(employment0206_p, age > 17 & age < 31 & year == 2002 | year == 2006),
+  feols(tal ~ i(as.factor(Female), provtariff) | year + ivid,
+        subset(employment0204_p, age > 30 & year == 2002 | year == 2004),
         vcov = ~tinh,
-        weights = ~hhwt)))
+        weights = ~hhwt)),
+  cex = 1.3, xlab = "Female")
 
+legend("bottomleft", col = 1:2, pch = 16, cex = 0.5,
+       legend = c("18-30", "31-65"))
+
+
+# Education 
 iplot(list(
   feols(tal ~ i(Female, provtariff) | year + ivid,
         subset(employment0204_p, educ > 9 & year == 2002 | year == 2004),
