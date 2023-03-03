e315daf2dfda8ba1644f0b13c9fbdadecc6e1aa5
Anri Sakakibara <anrisakaki@gmail.com>
2023-03-03 15:09:13 +0000

    Coefficient plots for household expenditure

--- Coefficient plots for household expenditure.R
+++ Coefficient plots for household expenditure.R
@@ -11,7 +11,9 @@ coefplot(list(
         exp_0402_p,
         vcov = ~tinh,
         weights = ~hhwt)  
-), main = "")
+), main = "", zero.par = list( type="dotted", lty=2))
+legend("bottomleft", col = 1:3, pch = 1, lwd = 2, cex = 1, bty = "n", 
+       legend = c("Food", "Education", "Health"))
 
 coefplot(list(
   feols(food_share ~ provtariff_k | hhid02 + yearint,
@@ -26,5 +28,19 @@ coefplot(list(
         exp_0206_p,
         vcov = ~tinh,
         weights = ~hhwt)  
-), main = "")
+), main = "", zero.par = list( type="dotted", lty=2))
+legend("bottomleft", col = 1:3, pch = 1, lwd = 2, cex = 1, bty = "n", 
+       legend = c("Food", "Education", "Health"))
 
+coefplot(list(
+  feols(tobac_share ~ provtariff_k | hhid02 + year,
+        exp_0402_p,
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(tobac_share ~ provtariff_k | hhid02 + yearint,
+        exp_0206_p,
+        vcov = ~tinh,
+        weights = ~hhwt)
+), main = "", zero.par = list( type="dotted", lty=2))
+legend("bottomleft", col = 1:2, pch = 1, lwd = 2, cex = 1, bty = "n", 
+       legend = c("2001-2003", "2001-2005"))
