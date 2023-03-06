f2e95b6204712ac00f1f4a7f5fbe867577a44831
Anri Sakakibara <anrisakaki@gmail.com>
2023-03-06 14:25:42 +0000

    Coefficient plots for household expenditure

--- Coefficient plots for household expenditure.R
+++ Coefficient plots for household expenditure.R
@@ -15,6 +15,7 @@ coefplot(list(
 legend("bottomleft", col = 1:3, pch = 1, lwd = 2, cex = 1, bty = "n", 
        legend = c("Food", "Education", "Health"))
 
+png("tce_hhexp_0206.png")
 coefplot(list(
   feols(food_share ~ provtariff_k | hhid02 + yearint,
         exp_0206_p,
@@ -27,10 +28,38 @@ coefplot(list(
   feols(health_share ~ provtariff_k | hhid02 + yearint,
         exp_0206_p,
         vcov = ~tinh,
+        weights = ~hhwt),
+  feols(tobac_share ~ provtariff_k | hhid02 + yearint,
+        exp_0206_p,
+        vcov = ~tinh,
         weights = ~hhwt)  
 ), main = "", zero.par = list( type="dotted", lty=2))
-legend("bottomleft", col = 1:3, pch = 1, lwd = 2, cex = 1, bty = "n", 
-       legend = c("Food", "Education", "Health"))
+legend("bottomleft", col = 1:4, pch = 1, lwd = 2, cex = 1, bty = "n", 
+       legend = c("Food", "Education", "Health", "Tobacco"))
+dev.off()
+
+png("tce_hhexp_real_0206.png")
+coefplot(list(
+  feols(log(foodreal) ~ provtariff_k | hhid02 + yearint,
+        exp_0206_p,
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(log(educex_2) ~ provtariff_k | hhid02 + yearint,
+        exp_0206_p,
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(log(hlthex_2) ~ provtariff_k | hhid02 + yearint,
+        exp_0206_p,
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(log(tobac12m) ~ provtariff_k | hhid02 + yearint,
+        exp_0206_p,
+        vcov = ~tinh,
+        weights = ~hhwt)  
+), main = "", zero.par = list( type="dotted", lty=2))
+legend("bottomleft", col = 1:4, pch = 1, lwd = 2, cex = 1, bty = "n", 
+       legend = c("Food", "Education", "Health", "Tobacco"))
+dev.off()
 
 coefplot(list(
   feols(tobac_share ~ provtariff_k | hhid02 + year,
@@ -44,3 +73,10 @@ coefplot(list(
 ), main = "", zero.par = list( type="dotted", lty=2))
 legend("bottomleft", col = 1:2, pch = 1, lwd = 2, cex = 1, bty = "n", 
        legend = c("2001-2003", "2001-2005"))
+
+png("tce_educexp_0206.png")
+iplot(feols(log(educ_exp) ~ i(as.factor(Female), provtariff_k) | ivid02 + year,
+                 schooling_0206_p,
+                 weights = ~hhwt,
+                 vcov = ~tinh), main = "", zero.par = list( type="dotted", lty=2))
+dev.off()
