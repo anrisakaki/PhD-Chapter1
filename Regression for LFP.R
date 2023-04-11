c71cdea5922cd62deba5c2bc14649963cbd6fbd8
Anri Sakakibara <anrisakaki@gmail.com>
2023-04-11 13:34:12 +0100

    Regression for LFP

--- Regression for LFP.R
+++ Regression for LFP.R
@@ -3,19 +3,19 @@
 #############################################################
 
 etable(list(
-  feols(work ~ as.factor(Female)/provtariff | ivid + year,
+  feols(work ~ i(as.factor(Female), provtariff) | ivid + year,
         employment0204_p,
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(work ~ as.factor(Female)/provtariff_k | ivid + year,
+  feols(work ~ i(as.factor(Female), provtariff_k) | ivid + year,
         employment0204_p,
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(work ~ as.factor(Female)/provtariff | ivid02 + year,
+  feols(work ~ i(as.factor(Female), provtariff) | ivid02 + year,
         employment0206_p,
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(work ~ as.factor(Female)/provtariff_k | ivid02 + year,
+  feols(work ~ i(as.factor(Female), provtariff_k) | ivid02 + year,
         employment0206_p,
         vcov = ~tinh,
         weights = ~hhwt)  
@@ -23,40 +23,39 @@ etable(list(
 
 # Rural 
 etable(list(
-  feols(work ~ as.factor(Female)/provtariff | ivid + year,
+  feols(work ~ i(as.factor(Female), provtariff) | ivid + year,
         subset(employment0204_p, urban == 2),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(work ~ as.factor(Female)/provtariff_k | ivid + year,
+  feols(work ~ i(as.factor(Female), provtariff_k) | ivid + year,
         subset(employment0204_p, urban == 2),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(work ~ as.factor(Female)/provtariff | ivid02 + year,
+  feols(work ~ i(as.factor(Female), provtariff) | ivid02 + year,
         subset(employment0206_p, urban == 2),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(work ~ as.factor(Female)/provtariff_k | ivid02 + year,
+  feols(work ~ i(as.factor(Female), provtariff_k) | ivid02 + year,
         subset(employment0206_p, urban == 2),
         vcov = ~tinh,
         weights = ~hhwt)  
 ), tex = TRUE)
 
-# Education 
 etable(list(
-  feols(work ~ as.factor(Female)/provtariff | ivid + year,
-        subset(employment0204_p, educ < 10 & year == 2002 | year == 2004),
+  feols(work ~ i(as.factor(Female), provtariff) | ivid + year,
+        subset(employment0204_p, urban == 1),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(work ~ as.factor(Female)/provtariff_k | ivid + year,
-        subset(employment0204_p, educ < 10 & year == 2002 | year == 2004),
+  feols(work ~ i(as.factor(Female), provtariff_k) | ivid + year,
+        subset(employment0204_p, urban == 1),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(work ~ as.factor(Female)/provtariff | ivid02 + year,
-        subset(employment0206_p, educ < 10 & year == 2002 | year == 2006),
+  feols(work ~ i(as.factor(Female), provtariff) | ivid02 + year,
+        subset(employment0206_p, urban == 1),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(work ~ as.factor(Female)/provtariff_k | ivid02 + year,
-        subset(employment0206_p, educ < 10 & year == 2002 | year == 2006),
+  feols(work ~ i(as.factor(Female), provtariff_k) | ivid02 + year,
+        subset(employment0206_p, urban == 1),
         vcov = ~tinh,
         weights = ~hhwt)  
 ), tex = TRUE)
