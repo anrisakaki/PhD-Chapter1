db8e938a8c2fbef1204fe5f2ab2dd111dad9a3ae
Anri Sakakibara <anrisakaki@gmail.com>
2023-04-12 17:46:11 +0100

    Coefficient plots for income and spousal wage gap

--- Coefficient plots for income and spousal wage gap.R
+++ Coefficient plots for income and spousal wage gap.R
@@ -5,11 +5,11 @@ setFixest_coefplot(dict = dict, grid = F)
 png("tce_finc_020406.png")
 coefplot(list(
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0204_spouse_p, Female == 1),
+        subset(inc_0204_spouse_p, Female == 1 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh),
   feols(inc_ratio ~ provtariff_k | hhid06 + year,
-        subset(inc_0206_spouse_p, Female == 1),
+        subset(inc_0206_spouse_p, Female == 1 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh)   
 ), main = "")
@@ -22,11 +22,11 @@ dev.off()
 png("tce_finc_urban_0204.png")
 coefplot(list(
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0204_spouse_p, Female == 1 & urban == 1),
+        subset(inc_0204_spouse_p, Female == 1 & urban == 1 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh),
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0204_spouse_p, Female == 1 & urban == 2),
+        subset(inc_0204_spouse_p, Female == 1 & urban == 2 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh)
 ), main = "", zero.par = list( type="dotted", lty=2))
@@ -37,11 +37,11 @@ dev.off()
 png("tce_finc_urban_0206.png")
 coefplot(list(
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0206_spouse_p, Female == 1 & urban == 1),
+        subset(inc_0206_spouse_p, Female == 1 & urban == 1 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh),
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0206_spouse_p, Female == 1 & urban == 2),
+        subset(inc_0206_spouse_p, Female == 1 & urban == 2 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh)
 ), main = "", zero.par = list( type="dotted", lty=2))
@@ -53,15 +53,15 @@ dev.off()
 png("tce_finc_educ_0204.png")
 coefplot(list(
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0204_spouse_p, Female == 1 & educ > 9 & year == 2002 | year == 2004),
+        subset(inc_0204_spouse_p, Female == 1 & educ > 9 & year == 2002 & married == 2 | year == 2004 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh),
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0204_spouse_p, Female == 1 & educ > 5 & educ < 10 & year == 2002 | year == 2004),
+        subset(inc_0204_spouse_p, Female == 1 & educ > 5 & educ < 10 & year == 2002 & married == 2 | year == 2004 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh),
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0204_spouse_p, Female == 1 & educ < 6 & year == 2002 | year == 2004),
+        subset(inc_0204_spouse_p, Female == 1 & educ < 6 & year == 2002 & married == 2 | year == 2004 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh)), main = "", zero.par = list( type="dotted", lty=2))
 legend("bottomleft", col = 1:3, pch = 16, lwd = 2, bty = "n", cex = 0.9,
@@ -71,15 +71,15 @@ dev.off()
 png("tce_finc_educ_0206.png")
 coefplot(list(
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0206_spouse_p, Female == 1 & educ > 9 & year == 2002 | year == 2006),
+        subset(inc_0206_spouse_p, Female == 1 & educ > 9 & year == 2002 & married == 2 | year == 2006 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh),
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0206_spouse_p, Female == 1 & educ > 5 & educ < 10 & year == 2002 | year == 2006),
+        subset(inc_0206_spouse_p, Female == 1 & educ > 5 & educ < 10 & year == 2002 & married == 2 | year == 2006 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh),
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0206_spouse_p, Female == 1 & educ < 6 & year == 2002 | year == 2006),
+        subset(inc_0206_spouse_p, Female == 1 & educ < 6 & year == 2002 & married == 2 | year == 2006 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh)), main = "")
 legend("bottomleft", col = 1:3, pch = 16, lwd = 2, bty = "n", cex = 0.9,
@@ -90,11 +90,11 @@ dev.off()
 png("tce_finc_age_0204.png")
 coefplot(list(
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0204_spouse_p, age > 17 & age < 31 & year == 2002 | year == 2004),
+        subset(inc_0204_spouse_p, age > 17 & age < 31 & year == 2002 & married == 2 | year == 2004 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh),
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0204_spouse_p, age > 30 & year == 2002 | year == 2004),
+        subset(inc_0204_spouse_p, age > 30 & year == 2002 & married == 2 | year == 2004 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh)
 ), main = "", zero.par = list( type="dotted", lty=2))
@@ -105,11 +105,11 @@ dev.off()
 png("tce_finc_age_0206.png")
 coefplot(list(
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0206_spouse_p, age > 17 & age < 31 & year == 2002 | year == 2006),
+        subset(inc_0206_spouse_p, age > 17 & age < 31 & year == 2002 & married == 2 | year == 2006 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh),
   feols(inc_ratio ~ provtariff_k | hhid02 + year,
-        subset(inc_0206_spouse_p, age > 30 & year == 2002 | year == 2006),
+        subset(inc_0206_spouse_p, age > 30 & year == 2002 & married == 2 | year == 2006 & married == 2),
         weights = ~hhwt, 
         vcov = ~tinh)
 ), main = "", zero.par = list( type="dotted", lty=2))
