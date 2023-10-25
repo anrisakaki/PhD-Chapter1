d95192e6cac4d8f8499a4942c7647e7012094117
Anri Sakakibara <anrisakaki@gmail.com>
2023-10-25 21:40:31 +0100

    Weights data

--- Weights data.R
+++ Weights data.R
@@ -6,8 +6,7 @@
 weights_02 <- inc_02 %>%
   select(tinh02, xa02, diaban02, huyen02, wt75, urban) %>% #Weights are determined by tinh, xa, diaban, huyen
   rename(hhwt = wt75) %>% 
-  distinct() %>% 
-  mutate(xa02 = tinh02 * 10^4 + huyen02 * 10^2 + xa02)
+  distinct()
 
 weights_exp_02 <- exp_02 %>% 
   select(tinh02, xa02, hhid, wt30) %>% 
