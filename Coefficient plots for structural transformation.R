ff2d34843f6d06e7a17e6abbcd7957deb06a80c6
Anri Sakakibara <anrisakaki@gmail.com>
2023-11-04 09:54:27 +0000

    Coefficient plots for structural transformation

--- Coefficient plots for structural transformation.R
+++ Coefficient plots for structural transformation.R
@@ -2,18 +2,18 @@
 # PLOTTING REALLOCATION INTO THE WEARING APPAREL AND LEATHER SECTOR # 
 #####################################################################
 
-dict = c("as.factor(Female)" = "Female")
+dict = c("as.factor(female)" = "Female")
 
 setFixest_coefplot(dict = dict, grid = F, zero.par = list( type="dotted", lty=2), main = "")
 
 png("reallocation_lpm_020406.png")
 iplot(list(
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid,
-        employment0204_p,
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        emp0204_p,
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid02,
-        employment0206_p,
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        emp0206_p,
         vcov = ~tinh,
         weights = ~hhwt)), zero = F, main = "")
 legend("bottomleft", col = 1:2, pch = 1, lwd = 2, cex = 0.7, bty = "n", 
@@ -23,12 +23,12 @@ dev.off()
 # Age 
 png("reallocation_lpm_age_0204.png")
 iplot(list(
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid,
-        subset(employment0204_p, age > 15 & age < 31 & year == 2002 | year == 2004),
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        subset(emp0204_p, age > 15 & age < 31 & year == 2002 | year == 2004),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid,
-        subset(employment0204_p, age > 30 & year == 2002 | year == 2004),
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        subset(emp0206_p, age > 30 & year == 2002 | year == 2004),
         vcov = ~tinh,
         weights = ~hhwt)), zero = F, main = "")
 legend("bottomleft", col = 1:2, pch = 16, cex = 0.9, bty = "n",
@@ -37,12 +37,12 @@ dev.off()
 
 png("reallocation_lpm_age_0206.png")
 iplot(list(
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid02,
-        subset(employment0206_p, age > 15 & age < 31 & year == 2002 | year == 2006),
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        subset(emp0204_p, age > 15 & age < 31 & year == 2002 | year == 2006),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid02,
-        subset(employment0206_p, age > 30 & year == 2002 | year == 2006),
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        subset(emp0206_p, age > 30 & year == 2002 | year == 2006),
         vcov = ~tinh,
         weights = ~hhwt)), main = "")
 legend("bottomleft", col = 1:2, pch = 16, bty = "n", cex = 0.9, 
@@ -52,16 +52,16 @@ dev.off()
 # Education 
 png("reallocation_lpm_educ_0204.png")
 iplot(list(
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid,
-        subset(employment0204_p, educ > 9 & year == 2002 | year == 2004),
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        subset(emp0204_p, educ > 9 & year == 2002 | year == 2004),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid,
-        subset(employment0204_p, educ > 5 & educ < 10 & year == 2002 | year == 2004),
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        subset(emp0204_p, educ > 5 & educ < 10 & year == 2002 | year == 2004),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid,
-        subset(employment0204_p, educ < 6 & year == 2002 | year == 2004),
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        subset(emp0204_p, educ < 6 & year == 2002 | year == 2004),
         vcov = ~tinh,
         weights = ~hhwt)), main = "")
 legend("bottomleft", col = 1:3, pch = 16, bty = "n", cex = 0.9,
@@ -70,16 +70,16 @@ dev.off()
 
 png("reallocation_lpm_educ_0206.png")
 iplot(list(
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid02,
-        subset(employment0206_p, educ > 9 & year == 2002 | year == 2006),
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        subset(emp0206_p, educ > 9 & year == 2002 | year == 2006),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid02,
-        subset(employment0206_p, educ > 5 & educ < 10 & year == 2002 | year == 2006),
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        subset(emp0206_p, educ > 5 & educ < 10 & year == 2002 | year == 2006),
         vcov = ~tinh,
         weights = ~hhwt),
-  feols(tal ~ i(as.factor(Female), -provtariff) | year + ivid02,
-        subset(employment0206_p, educ < 6 & year == 2002 | year == 2006),
+  feols(tal ~ i(as.factor(female), -provtariff) | year + ivid,
+        subset(emp0206_p, educ < 6 & year == 2002 | year == 2006),
         vcov = ~tinh,
         weights = ~hhwt)), main = "")
 legend("bottomleft", col = 1:3, pch = 16, bty = "n", cex = 0.9,
