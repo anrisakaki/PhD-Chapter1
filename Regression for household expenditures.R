8178c543694bf65dcf87f3a6495c0b8244dced5c
Anri Sakakibara <anrisakaki@gmail.com>
2023-11-04 22:37:06 +0000

    Regression for HH expenditure and enrollment of children in school

--- Regression for household expenditures.R
+++ Regression for household expenditures.R
@@ -13,9 +13,9 @@ etable(list(
         weights = ~hhwt)  
 ))
 
-########################################################################
-# REGRESSION ON EXPENDITURE ON HOUSEHOLD PUBLIC GOODS USING PANEL DATA #
-########################################################################
+#######################################################
+# REGRESSION ON EXPENDITURE ON HOUSEHOLD PUBLIC GOODS #
+#######################################################
 
 etable(list(
   feols(food_share ~ provtariff | hhid + year,
@@ -50,5 +50,79 @@ etable(list(
         data = exp0206_p,
         vcov = ~tinh,
         weights = ~hhwt)  
+), tex = T)
+
+etable(list(
+  feols(log(foodreal) ~ provtariff | hhid + year,
+        data = exp0204_p,
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(log(educex_2) ~ provtariff | hhid + year,
+        data = exp0204_p,
+        vcov = ~tinh,
+        weights = ~hhwt),  
+  feols(log(hlthex_2) ~ provtariff | hhid + year,
+        data = exp0204_p,
+        vcov = ~tinh,
+        weights = ~hhwt),   
+  feols(log(tobac12m) ~ provtariff | hhid + year,
+        data = exp0204_p,
+        vcov = ~tinh,
+        weights = ~hhwt),   
+  feols(log(foodreal) ~ provtariff | hhid + year,
+        data = exp0206_p,
+        vcov = ~tinh,
+        weights = ~hhwt),
+  feols(log(educex_2) ~ provtariff | hhid + year,
+        data = exp0206_p,
+        vcov = ~tinh,
+        weights = ~hhwt),  
+  feols(log(hlthex_2) ~ provtariff | hhid + year,
+        data = exp0206_p,
+        vcov = ~tinh,
+        weights = ~hhwt),   
+  feols(log(tobac12m) ~ provtariff | hhid + year,
+        data = exp0206_p,
+        vcov = ~tinh,
+        weights = ~hhwt) 
+), tex = T)
+
+# Education per child 
+
+etable(list(
+  feols(enrolled ~ i(as.factor(female), provtariff) | hhid + year,
+        school0204_p,
+        weights = ~hhwt,
+        vcov = ~tinh),
+  feols(enrolled ~ i(as.factor(female), provtariff) | hhid + year,
+        school0206_p,
+        weights = ~hhwt,
+        vcov = ~tinh),
+  feols(enrolled ~ i(as.factor(female), provtariff) | hhid + year,
+        subset(school0204_p, age > 16),
+        weights = ~hhwt,
+        vcov = ~tinh),
+  feols(enrolled ~ i(as.factor(female), provtariff) | hhid + year,
+        subset(school0206_p, age > 16),
+        weights = ~hhwt,
+        vcov = ~tinh)  
 ))
 
+etable(list(
+  feols(log(educ_exp) ~ i(as.factor(female), provtariff) | hhid + year,
+        school0204_p,
+        weights = ~hhwt,
+        vcov = ~tinh),
+  feols(log(educ_exp) ~ i(as.factor(female), provtariff) | hhid + year,
+        school0206_p,
+        weights = ~hhwt,
+        vcov = ~tinh),
+  feols(log(educ_exp) ~ i(as.factor(female), provtariff) | hhid + year,
+        subset(school0204_p, age > 11),
+        weights = ~hhwt,
+        vcov = ~tinh),
+  feols(log(educ_exp) ~ i(as.factor(female), provtariff) | hhid + year,
+        subset(school0206_p, age > 11),
+        weights = ~hhwt,
+        vcov = ~tinh)  
+))
