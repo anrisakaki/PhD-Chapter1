c5e3cc38a5fe950d13f37c9b233ae89a8ce5f0a3
Anri Sakakibara <anrisakaki@gmail.com>
2023-11-04 12:02:35 +0000

    Expenditure data

--- Expenditure data.R
+++ Expenditure data.R
@@ -16,13 +16,16 @@ for(i in exp_020406){
 
 exp_02 <- exp_02 %>% 
   mutate(year = 2002) %>% 
-  select(year, tinh02, huyen02, xa02, diaban02, hoso02, riceexp_share, food_share, tobac_share, educ_share, health_share, riceexp, foodreal, tobac12m, educex_2, hlthex_2, urban02, wt30) %>% 
+  select(year, tinh02, huyen02, xa02, hoso02, riceexp_share, food_share, tobac_share, educ_share, health_share, riceexp, foodreal, tobac12m, educex_2, hlthex_2, urban02, wt30, hhexp2rl) %>% 
   rename_with(~ str_replace(.x, "02", ""), everything()) %>% 
   rename(hhwt = wt30)
 
+exp_02 <- inner_join(exp_02, diaban02, by = c("tinh" = "tinh02", "huyen" = "huyen02", "xa" = "xa02")) %>% 
+  rename(diaban = diaban02)
+
 exp_04 <- exp_04 %>% 
   mutate(year = 2004) %>% 
-  select(year, tinh, huyen, xa, hoso, riceexp_share, food_share, tobac_share, educ_share, health_share, riceexp, foodreal, tobac12m, educex_2, hlthex_2, urban04, hhwt) %>% 
+  select(year, tinh, huyen, xa, hoso, riceexp_share, food_share, tobac_share, educ_share, health_share, riceexp, foodreal, tobac12m, educex_2, hlthex_2, urban04, hhwt, hhexp2rl) %>% 
   rename(urban = urban04)
 
 exp_04 <- merge(exp_04, diaban04, by = c("tinh", "huyen", "xa")) %>% 
@@ -30,22 +33,21 @@ exp_04 <- merge(exp_04, diaban04, by = c("tinh", "huyen", "xa")) %>%
 
 exp_06 <- exp_06 %>% 
   mutate(year = 2006) %>% 
-  select(year, tinh, huyen, xa, hoso, riceexp_share, food_share, tobac_share, educ_share, health_share, riceexp, foodreal, tobac12m, educex_2, hlthex_2, urban06, wt9) %>% 
+  select(year, tinh, huyen, xa, hoso, riceexp_share, food_share, tobac_share, educ_share, health_share, riceexp, foodreal, tobac12m, educex_2, hlthex_2, urban06, wt9, hhexp2rl) %>% 
   rename(urban = urban06,
          hhwt = wt9)
 
 exp_06 <- inner_join(exp_06, diaban06, by = c("tinh" = "tinh06", "huyen" = "huyen06", "xa" = "xa06")) %>% 
   rename(diaban = diaban06)
 
-exp0204 <- bind_rows(exp_02, exp_04)
-exp0206 <- bind_rows(exp_02, exp_06)
+exp0204 <- bind_rows(exp_02, exp_04) %>% select(tinh, huyen, xa, diaban, hoso, everything())
+exp0206 <- bind_rows(exp_02, exp_06) %>% select(tinh, huyen, xa, diaban, hoso, everything())
 
 exp0204 <- merge(exp0204, provtariffs0204, by = c("tinh", "year"))
-exp0206 <- merge(exp0204, provtariffs0206, by = c("tinh", "year"))
+exp0206 <- merge(exp0206, provtariffs0206, by = c("tinh", "year"))
 
 exp0204_p <- merge(panel0204, exp0204, by = hhid)
-exp0206_p <- merge(panel0204, exp0206, by = hhid)
-
+exp0206_p <- merge(panel0206, exp0206, by = hhid)
 
 save(exp0204_p, file = "exp0204_p.rda")
 save(exp0206_p, file = "exp0206_p.rda")
