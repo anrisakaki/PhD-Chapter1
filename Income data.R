36e02bf401ed0020c394b21eb0a8278787c5be36
Anri Sakakibara <anrisakaki@gmail.com>
2023-11-06 08:29:15 +0000

    Income data

--- Income data.R
+++ Income data.R
@@ -33,9 +33,40 @@ hhinc0206_p <- merge(hhinc0206_p, weights_06, by = c("tinh", "huyen", "xa"))
 
 emp0204_p <- merge(emp0204_p, hhinc0204, by = hhid) %>%
   mutate(inc_share = inc/hhinc, 
-         inc_share = ifelse(inc_share >= 1 | inc_share < 0, NA, inc_share))
+         inc_share = ifelse(inc_share >= 1 | inc_share < 0, NA, inc_share),
+         inc_share = ifelse(is.na(inc_share), 0, inc_share)) %>%
+  group_by(hhid, year) %>% 
+  mutate(tot_inc_share = sum(inc_share),
+         agri_impute = ifelse(agri_work == 1 & tot_inc_share == 0, 0.5, inc_share),
+         inc_impute = ifelse(wage_work == 1 & inc_share == 0 | self_bus == 1 & inc_share == 0 | self_agri == 1 & inc_share == 0, 1 - tot_inc_share, agri_impute),
+         inc_share = ifelse(inc_share == 0, NA, inc_share),
+         inc_impute = ifelse(inc_impute == 0, NA, inc_impute),
+         wider = ifelse(any(relationship > 3), 1, 0),
+         spouse = ifelse(relationship == 1 | relationship == 2, 1, 0)) %>% 
+  ungroup() %>% 
+  select(-c(tot_inc_share, agri_impute))
 
 emp0206_p <- merge(emp0206_p, hhinc0206, by = hhid) %>%
   mutate(inc_share = inc/hhinc, 
-         inc_share = ifelse(inc_share >= 1 | inc_share < 0, NA, inc_share))
+         inc_share = ifelse(inc_share >= 1 | inc_share < 0, NA, inc_share),
+         inc_share = ifelse(is.na(inc_share), 0, inc_share)) %>%
+  group_by(hhid, year) %>% 
+  mutate(tot_inc_share = sum(inc_share),
+         agri_impute = ifelse(agri_work == 1 & tot_inc_share == 0, 0.5, inc_share),
+         inc_impute = ifelse(wage_work == 1 & inc_share == 0 | self_bus == 1 & inc_share == 0 | self_agri == 1 & inc_share == 0, 1 - tot_inc_share, agri_impute),
+         inc_share = ifelse(inc_share == 0, NA, inc_share),
+         inc_impute = ifelse(inc_impute == 0, NA, inc_impute),
+         wider = ifelse(any(relationship > 3), 1, 0),
+         spouse = ifelse(relationship == 1 | relationship == 2, 1, 0)) %>% 
+  ungroup() %>% 
+  select(-c(tot_inc_share, agri_impute))
   
+# Horowitz-Manski Bounds 
+
+hm_upperbound_02 <- max(emp0204_p$inc_share[emp0204_p$year == 2002], na.rm = T)
+hm_upperbound_04 <- max(emp0204_p$inc_share[emp0204_p$year == 2004], na.rm = T)
+hm_upperbound_06 <- max(emp0206_p$inc_share[emp0206_p$year == 2006], na.rm = T)
+
+hm_lowerbound_02 <- min(emp0204_p$inc_share[emp0204_p$year == 2002], na.rm = T)
+hm_lowerbound_04 <- min(emp0204_p$inc_share[emp0204_p$year == 2004], na.rm = T)
+hm_lowerbound_06 <- min(emp0206_p$inc_share[emp0206_p$year == 2006], na.rm = T)
