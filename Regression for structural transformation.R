76f7310d7b91c65cc97bdafbde1034730a64502d
Anri Sakakibara <anrisakaki@gmail.com>
2025-02-10 14:14:17 +0000

    Regression for structural transformation

--- Regression for structural transformation.R
+++ Regression for structural transformation.R
@@ -2,54 +2,46 @@
 # SETTING UP FOR REGRESSION ON STRUCTURAL TRANSFORMATION #
 ##########################################################
 
-vhlss0204 <- bind_rows(vhlss02, vhlss04)
-vhlss0206 <- bind_rows(vhlss02, vhlss06)
+provrecode_fn <- function(i){
+  i %>% 
+    mutate(tinh = ifelse(tinh == 302, 301, tinh),
+           tinh = ifelse(tinh == 606, 605, tinh),
+           tinh = ifelse(tinh == 816, 815, tinh))
+}
 
-vhlss0204 <- merge(vhlss0204, bta0204, by = c("tinh", "year"))
-vhlss0206 <- merge(vhlss0206, bta0206, by = c("tinh", "year"))
+emp0204_p <- bind_rows(vhlss02, vhlss04) %>%
+  merge(ivid0204, by = ivid) %>%
+  provrecode_fn() %>%
+  left_join(bta0204, by = c("tinh", "year")) %>% 
+  mutate(tariff = tariff*-1,
+         tariff_f = tariff_f*-1)
 
-emp0204_p <- merge(vhlss0204, ivid0204, by = ivid)
-
-emp0206_p <- merge(vhlss0206, ivid0206, by = ivid)
-
-# write .rda files 
-save(employment_0204, file='employment0204.rda')
-save(emp0204_p, file='emp0204_p.rda')
-save(employment_0206, file = 'employment0206.rda')
-save(emp0206_p, file = 'emp0206_p.rda')
+emp0206_p <- bind_rows(vhlss02, vhlss06) %>%
+  merge(ivid0206, by = ivid) %>%
+  provrecode_fn() %>% 
+  left_join(bta0206, by = c("tinh", "year")) %>% 
+  mutate(tariff = tariff*-1,
+         tariff_f = tariff_f*-1)
 
 ############################################################
 # REGRESSION ON STRUCTURAL TRANSFORMATION USING PANEL DATA #
 ############################################################
-setFixest_dict(c("as.factor(female)" = "Female",
-                 "-provtariff" = "Tariff_{pt}",
-                 "-provtariff_f" = "Tariff_{pt}^f",
-                 "as.factor(female)0" = "",
-                 "as.factor(female)1" = "Female",
-                 "year" = "Year",
-                 "ivid" = "Individual",
-                 "hhid" = "Household",
-                 "ivid02" = "Individual",
-                 "hhid02" = "Household",
-                 "tal" = "Wearing apparel and leather"))
-
-df <- list(emp0204_p, emp0206_p)
 
 etable(list(
-  feols(agri ~ -tariff + i(as.factor(female), -tariff) | year + ivid,
-        subset(emp0204_p),
+  feols(agri ~ i(as.factor(female), tariff) | year + ivid,
+        subset(emp0204_p, work == 1),
         weights = ~hhwt,
         vcov = ~tinh),
-  feols(agri ~ -tariff + i(as.factor(female), -tariff) | year + ivid,
-        data = emp0206_p,
+  feols(agri ~ i(as.factor(female), tariff) | year + ivid,
+        subset(emp0206_p, work == 1),
         weights = ~hhwt,
         vcov = ~tinh),
-  feols(agri ~ -tariff + i(as.factor(female), -tariff_f) | year + ivid,
-        data = emp0204_p,
+  feols(agri ~ i(as.factor(female), tariff_f) | year + ivid,
+        subset(emp0204_p, work == 1),
         weights = ~hhwt,
         vcov = ~tinh),
-  feols(agri ~ -tariff + i(as.factor(female), -tariff_f) | year + ivid,
-        data = emp0206_p,
+  feols(agri ~ i(as.factor(female), tariff_f) | year + ivid,
+        subset(emp0206_p, work == 1),
         weights = ~hhwt,
-        vcov = ~tinh)  
+        vcov = ~tinh)
 ), tex = T)
