d74c9797e531fd2417863dad1b23220c4474ff7b
Anri Sakakibara <anrisakaki@gmail.com>
2025-02-10 15:55:12 +0000

    Employment data

--- Employment data.R
+++ Employment data.R
@@ -3,7 +3,6 @@
 ########################################################
 
 # 2002 
-
 vhlss02 <- list(m1_02, m2_02, m3_02) %>% 
   reduce(full_join, by = c("tinh", "xa", "hoso", "matv", "tinh02", "huyen02", "xa02", "diaban02", "hoso02", "matv02")) %>% 
   mutate(matv02 = ifelse(nchar(matv02) > 2, substr(matv02, nchar(matv02) - 1, nchar(matv02)), matv02),
@@ -18,15 +17,18 @@ vhlss02 <- list(m1_02, m2_02, m3_02) %>%
          divorce = ifelse(m1c6 == 4, 1, 0),
          agri = ifelse(m3c7 == 1 & work == 1, 1, 0),
          agri = ifelse(work == 0, NA, agri),
+         selfagri = ifelse(m3c1b == 1, 1, 0),
+         selfagri = ifelse(work == 0, NA, selfagri),
+         formal = ifelse(m3c8 == 7 | m3c8 == 9, 1, 0),
+         formal = ifelse(work == 0, NA, formal),
          female = ifelse(m1c2 == 2, 1, 0)) %>% 
   rename(educ = m2c1,
          industry = m3c7,
          age = m1c5,
          hours = m3c11,
-         sex = m1c2,
          days = m3c10) %>%
   filter(age > 15 & age < 65) %>%
-  select(tinh02, huyen02, xa02, diaban02, hoso02, matv02, female, divorce, educ, work, wage_work, agri, industry, age, married, days, hours, year, housework) %>% 
+  select(tinh02, huyen02, xa02, diaban02, hoso02, matv02, female, divorce, educ, age, married, work, agri, selfagri, industry, formal, days, hours, year, housework) %>% 
   merge(weights_02, by = c("tinh02", "xa02", "huyen02", "diaban02")) %>% 
   rename(tinh = tinh02, 
          huyen = huyen02,
@@ -52,9 +54,13 @@ vhlss04 <- left_join(m123a_04, m4a_04, by = c("tinh", "huyen", "xa", "hoso", "ma
          female = ifelse(sex == 2, 1, 0),
          agri = ifelse(industry == 1 & work == 1, 1, 0),
          agri = ifelse(work == 0, NA, agri),
+         selfagri = ifelse(m4ac1b == 1, 1, 0),
+         selfagri = ifelse(work == 0, NA, selfagri),
+         formal = ifelse(m4ac10a == 6 | m4ac10a== 7, 1, 0),
+         formal = ifelse(work == 0, NA, formal),
          divorce = ifelse(m4ac6 == 4, 1, 0)) %>% 
   filter(age > 15 & age < 65) %>%
-  select(tinh, huyen, xa, hoso, matv, divorce, female, educ, wage_work, work, agri, industry, age, married, days, hours, year, housework, female)
+  select(tinh, huyen, xa, hoso, matv, female, divorce, educ, age, married, work, agri, selfagri, industry, formal, days, hours, year, housework)
 
 vhlss04 <- list(vhlss04, diaban04, weights_04) %>% 
   reduce(merge, by = c("tinh", "huyen", "xa")) %>% 
@@ -76,9 +82,13 @@ vhlss06 <- list(m1a_06, m2a_06, m4a_06) %>%
          female = ifelse(m1ac2 == 2, 1, 0),
          agri = ifelse(industry == 1 & work == 1, 1, 0),
          agri = ifelse(work == 0, NA, agri),
+         selfagri = ifelse(m4ac1b == 1, 1, 0),
+         selfagri = ifelse(work == 0, NA, selfagri),
+         formal = ifelse(m4ac10a == 6 | m4ac10a== 7, 1, 0),
+         formal = ifelse(work == 0, NA, formal),
          divorce = ifelse(m1ac6 == 4, 1, 0)) %>% 
   filter(age > 15 & age < 65) %>%
-  select(tinh, huyen, xa, hoso, matv, divorce, female, educ, work, agri, industry, age, married, days, hours, year, housework) %>% 
+  select(tinh, huyen, xa, hoso, matv, female, divorce, educ, age, married, work, agri, selfagri, industry, formal, days, hours, year, housework) %>% 
   left_join(diaban06, by =c("tinh" = "tinh06", "huyen" = "huyen06", "xa" = "xa06")) %>% 
   left_join(weights_06, by = c("tinh", "huyen", "xa")) %>%
   rename(diaban = diaban06)
@@ -87,80 +97,86 @@ vhlss06 <- list(m1a_06, m2a_06, m4a_06) %>%
 # CREATING EMPLOMYMENT DUMMIES AND LABELLING INDUSTRIES #
 #########################################################
 
-emp020406 <- c("vhlss02", "vhlss04", "vhlss06")
-
-for(i in emp020406) {
-  assign(i,get(i) %>%
-           mutate(
-             apparel = ifelse(industry == 18, 1, 0),
-             service = ifelse(industry > 50 & industry < 60, 1, 0),
-             manu = as.numeric(industry > 14 & industry < 38),
-             construction = as.numeric(industry == 45),
-             hours = ifelse(work == 1 & hours == 0, NA, hours),
-             days = ifelse(work == 1 & days == 0, NA, hours)))
-  
-  assign(i,get(i) %>%
-           mutate(Industry = recode(industry,
-                                    "0" = "Unemployed",
-                                    "1" = "Agriculture, hunting and related service activities",
-                                    "2" = "Forestry, logging and related service activities",
-                                    "5" = "Fishing, operation of fish hatcheries and fish farms; service activities incidental to fishing",
-                                    "10" = "Mining of coal and lignite" ,
-                                    "11" = "Extraction of crude petroleum and natural gas; service activities incidental to oil and gas extraction excluding surveying",
-                                    "12" = "Mining of uranium and thorium ores",
-                                    "13" =  "Mining of metal ores",
-                                    "14" = "Other mining and quarrying",
-                                    "15" = "Manufacture of food products and beverages",
-                                    "16" = "Manufacture of tobacco products",
-                                    "17" = "Manufacture of textiles",
-                                    "18" = "Manufacture of wearing apparel",
-                                    "19" = "Manufacture of leather goods (including footwear)",
-                                    "20" = "Manufacture of wood and of products of wood",
-                                    "21" = "Manufacture of paper and paper products",
-                                    "22" = "Publishing, printing and reproduction of recorded media",
-                                    "23" = "Manufacture of coke, refined petroleum products and nuclear fuel",
-                                    "24" = "Manufacture of chemicals and chemical products",
-                                    "25" = "Manufacture of rubber and plastics products",
-                                    "26" = "Manufacture of other non-metallic mineral products",
-                                    "27" = "Manufacture of basic metals",
-                                    "28" = "Manufacture of fabricated metal products",
-                                    "29" = "Manufacture of not-yet-classified machinery and equipment",
-                                    "30" = "Manufacture of office, accounting and computing machinery",
-                                    "31" = "Manufacture of electrical machinery and apparatus n.e.c.",
-                                    "32" = "Manufacture of radio, television and communication equipment and apparatus",
-                                    "33" = "Manufacture of medical, precision and optical instruments, watches and clocks",
-                                    "34" = "Manufacture of motor vehicles, trailers and semi-trailers",
-                                    "35" = "Manufacture of other transport equipment",
-                                    "36" = "Manufacture of furniture; manufacturing n.e.c.",
-                                    "37" = "Recycling",
-                                    "40" = "Electricity, gas, steam and hot water supply",
-                                    "41" = "Collection, purification and distribution of water",
-                                    "45" = "Construction",
-                                    "50" = "Sale, maintenance and repair of motor vehicles and motorcycles; retail sale of automotive fuel",
-                                    "51" = "Wholesale trade and commission trade, except of motor vehicles and motorcycles",
-                                    "52" ="Retail trade, except of motor vehicles and motorcycles; repair of personal and household goods",
-                                    "55" = "Hotels and restaurants",
-                                    "60" = "Land transport; transport via pipelines",
-                                    "61" = "Water transport",
-                                    "62" = "Air transport",
-                                    "63" = "Supporting and auxiliary transport activities",
-                                    "64" = "Post and telecommunications",
-                                    "65" = "Financial intermediation",
-                                    "66" = "Insurance and pension funding, except compulsory social security",
-                                    "67" = "Activities auxiliary to financial intermediation",
-                                    "70" = "Scientific and industrial activities",
-                                    "71" = "Real estate activities",
-                                    "72" = "Renting of machinery and equipment without operator and of personal and household goods",
-                                    "73" = "Computer and related activities",
-                                    "74" = "Other business activities",
-                                    "75" = "Public administration and defence; compulsory social security",
-                                    "80" = "Education",
-                                    "85" = "Health and social work",
-                                    "90" = "Sporting and other recreational activities",
-                                    "91"= "Activities of the party, organisations and associations",
-                                    "92" = "Sewage and refuse disposal, sanitation and similar activities",
-                                    "93" = "Other service activities",
-                                    "95" = "Private households with employed persons",
-                                    "99" = "Extra-territorial organizations and bodies")))
+vhlss_fn <- function(i) {
+  i %>%
+    mutate(
+      apparel = ifelse(industry == 18, 1, 0),
+      service = ifelse(industry > 50 & industry < 60, 1, 0),
+      manu = as.numeric(industry > 14 & industry < 38),
+      construction = as.numeric(industry == 45),
+      hours = ifelse(work == 1 & hours == 0, NA, hours),
+      days = ifelse(work == 1 & days == 0, NA, days),
+      formal_manu = ifelse(manu == 1 & formal == 1, 1, 0),
+      formal_service = ifelse(service == 1 & formal == 1, 1, 0)
+    ) %>%
+    mutate(
+      Industry = recode(
+        as.character(industry),
+        "0" = "Unemployed",
+        "1" = "Agriculture, hunting and related service activities",
+        "2" = "Forestry, logging and related service activities",
+        "5" = "Fishing, operation of fish hatcheries and fish farms; service activities incidental to fishing",
+        "10" = "Mining of coal and lignite",
+        "11" = "Extraction of crude petroleum and natural gas; service activities incidental to oil and gas extraction excluding surveying",
+        "12" = "Mining of uranium and thorium ores",
+        "13" = "Mining of metal ores",
+        "14" = "Other mining and quarrying",
+        "15" = "Manufacture of food products and beverages",
+        "16" = "Manufacture of tobacco products",
+        "17" = "Manufacture of textiles",
+        "18" = "Manufacture of wearing apparel",
+        "19" = "Manufacture of leather goods (including footwear)",
+        "20" = "Manufacture of wood and of products of wood",
+        "21" = "Manufacture of paper and paper products",
+        "22" = "Publishing, printing and reproduction of recorded media",
+        "23" = "Manufacture of coke, refined petroleum products and nuclear fuel",
+        "24" = "Manufacture of chemicals and chemical products",
+        "25" = "Manufacture of rubber and plastics products",
+        "26" = "Manufacture of other non-metallic mineral products",
+        "27" = "Manufacture of basic metals",
+        "28" = "Manufacture of fabricated metal products",
+        "29" = "Manufacture of not-yet-classified machinery and equipment",
+        "30" = "Manufacture of office, accounting and computing machinery",
+        "31" = "Manufacture of electrical machinery and apparatus n.e.c.",
+        "32" = "Manufacture of radio, television and communication equipment and apparatus",
+        "33" = "Manufacture of medical, precision and optical instruments, watches and clocks",
+        "34" = "Manufacture of motor vehicles, trailers and semi-trailers",
+        "35" = "Manufacture of other transport equipment",
+        "36" = "Manufacture of furniture; manufacturing n.e.c.",
+        "37" = "Recycling",
+        "40" = "Electricity, gas, steam and hot water supply",
+        "41" = "Collection, purification and distribution of water",
+        "45" = "Construction",
+        "50" = "Sale, maintenance and repair of motor vehicles and motorcycles; retail sale of automotive fuel",
+        "51" = "Wholesale trade and commission trade, except of motor vehicles and motorcycles",
+        "52" = "Retail trade, except of motor vehicles and motorcycles; repair of personal and household goods",
+        "55" = "Hotels and restaurants",
+        "60" = "Land transport; transport via pipelines",
+        "61" = "Water transport",
+        "62" = "Air transport",
+        "63" = "Supporting and auxiliary transport activities",
+        "64" = "Post and telecommunications",
+        "65" = "Financial intermediation",
+        "66" = "Insurance and pension funding, except compulsory social security",
+        "67" = "Activities auxiliary to financial intermediation",
+        "70" = "Scientific and industrial activities",
+        "71" = "Real estate activities",
+        "72" = "Renting of machinery and equipment without operator and of personal and household goods",
+        "73" = "Computer and related activities",
+        "74" = "Other business activities",
+        "75" = "Public administration and defence; compulsory social security",
+        "80" = "Education",
+        "85" = "Health and social work",
+        "90" = "Sporting and other recreational activities",
+        "91" = "Activities of the party, organisations and associations",
+        "92" = "Sewage and refuse disposal, sanitation and similar activities",
+        "93" = "Other service activities",
+        "95" = "Private households with employed persons",
+        "99" = "Extra-territorial organizations and bodies"
+      )
+    )
 }
 
+vhlss02 <- vhlss_fn(vhlss02)
+vhlss04 <- vhlss_fn(vhlss04)
+vhlss06 <- vhlss_fn(vhlss06)
