ffba0228793aee70b7adf211c08c85f9c989e61b
Anri Sakakibara <anrisakaki@gmail.com>
2025-02-12 14:32:52 +0000

    Province-level tariff

--- Province level tariffs.R
+++ Province level tariffs.R
@@ -1,13 +1,32 @@
-provtariffs <- vhlss02 %>% 
-  filter(work == 1) %>% 
-  group_by(tinh, industry) %>% 
-  summarise(n = sum(hhwt),
-            f = sum(hhwt*(female == 1))) %>% 
+library(dplyr)
+
+phc99 <- phc99 %>%
+  mutate(tinh = case_when(
+           geo1_vn1999 == 219 ~ 104,
+           geo1_vn1999 == 223 ~ 106,
+           geo1_vn1999 == 503 ~ 505,
+           geo1_vn1999 == 505 ~ 507,
+           geo1_vn1999 == 507 ~ 503,
+           geo1_vn1999 == 703 ~ 607,
+           TRUE ~ geo1_vn1999 
+         ))
+
+traded_n <- phc99 %>% 
+  mutate(isic2 = as.double(str_sub(as.character(ind), 1, if_else(nchar(as.character(ind)) < 3, 1, 2)))) %>% 
+  left_join(traded, by = "isic2") %>% 
+  filter(traded == 1) %>% 
+  group_by(tinh) %>% 
+  summarise(traded_n = sum(perwt))
+
+provtariffs <- phc99 %>% 
+  mutate(isic2 = as.double(str_sub(as.character(ind), 1, if_else(nchar(as.character(ind)) < 3, 1, 2))),
+         female = ifelse(sex == 2, 1, 0)) %>% 
+  group_by(tinh, isic2) %>% 
+  summarise(n = sum(perwt),
+            f = sum(perwt[female == 1], na.rm = T)) %>% 
   mutate(share_f = f / n) %>% 
-  group_by(tinh) %>%
-  mutate(share = n / sum(n)) %>% 
-  rename(isic2 = industry) %>%
-  mutate(isic2 = as.double(isic2)) %>%
+  left_join(traded_n, by = "tinh") %>% 
+  mutate(share = n / traded_n) %>% 
   left_join(tariff, by = "isic2") %>% 
   mutate(
     preBTA = share * col2_ave_all,
@@ -25,19 +44,22 @@ provtariffs <- vhlss02 %>%
 
 provtariffs02 <- provtariffs %>%
   select(tinh, preprov_tariff, preprov_tariff_f) %>% 
-  rename(tariff = preprov_tariff,
+  rename(tinh_old = tinh,
+         tariff = preprov_tariff,
          tariff_f = preprov_tariff_f) %>% 
   mutate(year = 2002)
 
 provtariffs04 <- provtariffs %>%
   select(tinh, postprov_tariff, postprov_tariff_f) %>% 
-  rename(tariff = postprov_tariff,
+  rename(tinh_old = tinh,
+         tariff = postprov_tariff,
          tariff_f = postprov_tariff_f) %>% 
   mutate(year = 2004)
 
 provtariffs06 <- provtariffs %>%
   select(tinh, postprov_tariff, postprov_tariff_f) %>% 
-  rename(tariff = postprov_tariff,
+  rename(tinh_old = tinh,
+         tariff = postprov_tariff,
          tariff_f = postprov_tariff_f) %>%
   mutate(year = 2006)
 
