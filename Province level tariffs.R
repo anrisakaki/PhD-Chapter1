b6ee3b58f8e962220254acc8b10abd5a2202ac71
Anri Sakakibara <anrisakaki@gmail.com>
2025-02-15 12:05:43 +0000

    Province-level tariffs

--- Province level tariffs.R
+++ Province level tariffs.R
@@ -1,4 +1,5 @@
 phc99 <- phc99 %>%
+  filter(age > 15 & age < 65) %>% 
   mutate(tinh = case_when(
            geo1_vn1999 == 219 ~ 104,
            geo1_vn1999 == 223 ~ 106,
@@ -9,13 +10,6 @@ phc99 <- phc99 %>%
            TRUE ~ geo1_vn1999 
          ))
 
-traded_n <- phc99 %>% 
-  mutate(isic2 = as.factor(str_sub(as.character(ind), 1, if_else(nchar(as.character(ind)) < 3, 1, 2)))) %>% 
-  left_join(traded, by = "isic2") %>% 
-  filter(traded == 1) %>% 
-  group_by(tinh) %>% 
-  summarise(traded_n = sum(perwt))
-
 provtariffs <- phc99 %>% 
   mutate(isic2 = as.factor(str_sub(as.character(ind), 1, if_else(nchar(as.character(ind)) < 3, 1, 2))),
          female = ifelse(sex == 2, 1, 0)) %>% 
@@ -23,8 +17,8 @@ provtariffs <- phc99 %>%
   summarise(n = sum(perwt),
             f = sum(perwt[female == 1], na.rm = T)) %>% 
   mutate(share_f = f / n) %>% 
-  left_join(traded_n, by = "tinh") %>% 
-  mutate(share = n / traded_n) %>% 
+  group_by(tinh) %>% 
+  mutate(share = n / sum(n)) %>% 
   left_join(tariff, by = "isic2") %>% 
   mutate(
     preBTA = share * col2_ave_all,
@@ -40,11 +34,24 @@ provtariffs <- phc99 %>%
     postprov_tariff_f = sum(postBTA_f, na.rm = TRUE)
   )
 
+tariff02_mccaig <- provtariff_mccaig %>%
+  select(province, col2_ave_all01, col2_ave_all98) %>% 
+  rename(mccaig_bta = col2_ave_all01,
+         mccaig_bta98 = col2_ave_all98,
+         tinh_old = province) 
+
+tariff04_mccaig <- provtariff_mccaig %>%
+  select(province, mfn_ave_all04) %>% 
+  rename(mccaig_bta = mfn_ave_all04,
+         tinh_old = province) %>% 
+  mutate(mccaig_bta98 = mccaig_bta)
+
 provtariffs02 <- provtariffs %>%
   select(tinh, preprov_tariff, preprov_tariff_f) %>% 
   rename(tinh_old = tinh,
          tariff = preprov_tariff,
          tariff_f = preprov_tariff_f) %>% 
+  left_join(tariff02_mccaig, by = "tinh_old") %>% 
   mutate(year = 2002)
 
 provtariffs04 <- provtariffs %>%
@@ -52,14 +59,8 @@ provtariffs04 <- provtariffs %>%
   rename(tinh_old = tinh,
          tariff = postprov_tariff,
          tariff_f = postprov_tariff_f) %>% 
+  left_join(tariff04_mccaig, by = "tinh_old") %>% 
   mutate(year = 2004)
 
-provtariffs06 <- provtariffs %>%
-  select(tinh, postprov_tariff, postprov_tariff_f) %>% 
-  rename(tinh_old = tinh,
-         tariff = postprov_tariff,
-         tariff_f = postprov_tariff_f) %>%
-  mutate(year = 2006)
-
 bta0204 <- bind_rows(provtariffs02, provtariffs04)
-bta0206 <- bind_rows(provtariffs02, provtariffs06)
+
